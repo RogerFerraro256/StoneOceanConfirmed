5bd391c2fd2993c23bb9f9337777d6d83aff826e
roger <rogerferraro256@gmail.com>
2018-12-27 19:14:02 -0200

    fixing config again

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 //this is the twitter keys from my Twitter account StoneOceanConf, i will remove them
 //to people don't mess up, hope you understand...
 module.exports = {
-	consumer_key:         'am0imPdXtLZ0AeXd63LN94vOB',
-  	consumer_secret:      'H8vBPEsHwCrhF0E1cBG1KdG2lCJmIovdeILTw6ep305glL7x1E',
-  	access_token:         '1078382215552221185-J2Lm7PTakK8chURW1hfLW4Tvdazltt',
-  	access_token_secret:  '1rbIX9JmWwSOqcE7IrvFVrD5KL8pE0SWzN0xf96s9Y0oY',
+	consumer_key:         '',
+  	consumer_secret:      '',
+  	access_token:         '',
+  	access_token_secret:  '',
   	timeout_ms:           60*1000,  
   	strictSSL:            true,
 }
