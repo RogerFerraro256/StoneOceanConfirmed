e5ac993a9305f860dc1db8f2ddaee964afbc1dca
Rogério Salvadeo Vieira <rogerferraro256@gmail.com>
2019-09-10 15:12:06 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
-//this is the twitter keys from my Twitter account StoneOceanConf, i will remove them
-//to people don't mess up, hope you understand...
 module.exports = {
-	//to pick those things just create a developer account on Twitter
+	
 	consumer_key:         '',
   	consumer_secret:      '',
-  	//just pick those to access your account, without this
-  	// the bot will not work.
+	
   	access_token:         '',
   	access_token_secret:  '',
   	timeout_ms:           60*1000,  
